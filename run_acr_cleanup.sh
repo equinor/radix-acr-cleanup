92f2823933139fb0b466e47f122e973de62c749d
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-10-29 07:36:49 +0100

    No need to echo

--- run_acr_cleanup.sh
+++ run_acr_cleanup.sh
@@ -7,12 +7,6 @@ if [[ -z "${SP_SECRET}" ]]; then
   SP_SECRET=$(cat ${AZURE_CREDENTIALS} | jq -r '.password')
 fi
 
-echo ${PERIOD}
-echo ${REGISTRY}
-echo ${CLUSTER_TYPE}
-echo ${DELETE_UNTAGGED}
-echo ${DELETE_UNTAGGED}
-
 az login --service-principal -u ${SP_USER} -p ${SP_SECRET} --tenant ${TENANT}
 /radix-acr-cleanup/radix-acr-cleanup \
   --period=${PERIOD} \
