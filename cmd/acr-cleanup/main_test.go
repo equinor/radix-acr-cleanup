29e78e676c83d9b822f3662e205ab51b0edf7c69
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2021-02-02 15:16:57 +0100

    70365 - add grace period for manifests (#13)
    * manifest grace period
    
    * fix validation of execution window
    
    Co-authored-by: Nils Gustav Stråbø <nst@equinor.com>

---
+++ cmd/acr-cleanup/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/equinor/radix-acr-cleanup/pkg/manifest"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_isManifestWithinGracePeriod(t *testing.T) {
+	created, _ := time.Parse(time.RFC3339, "2010-01-01T15:00:00Z")
+	manifest := manifest.Data{Timestamp: created}
+
+	timeAfter, _ := time.Parse(time.RFC3339, "2010-01-01T16:00:00Z")
+	assert.True(t, isManifestWithinGracePeriod(manifest, timeAfter, 2*time.Hour))
+	assert.False(t, isManifestWithinGracePeriod(manifest, timeAfter, 0))
+
+	timeBefore, _ := time.Parse(time.RFC3339, "2010-01-01T14:00:00Z")
+	assert.True(t, isManifestWithinGracePeriod(manifest, timeBefore, 0))
+}
